db4f6269ae2cbde1ecd14e38c534234e5e1a6403
JSA272 <JSA272@nyu.edu>
2015-01-24 20:54:43 -0500

    finished both functions, tested and working

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions can efficiently store a matrix and its inverse by computing the inverse once and
+## caching it. It can then call the inverse from the cache instead of computing it every time.
 
-## Write a short comment describing this function
-## This function creates a special "matrix" object that can cache its inverse.
+## Using this function will create a list containing a set of functions. 
+## Calling set(y) will allow the user to reset the matrix to y, and tell the function that
+## a new inverse needs to be cached. Calling get() will return the matrix itself.
+## The getIn and setInv allow the cacheSolve() function below to check if an inverse
+## has been cached and to cache a inverse if needed.
 
 makeCacheMatrix <- function(x = matrix()) {
-  
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInv <- function(inverse) inv <<- inverse
+  getInv <- function() inv
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv) 
 }
 
+## This function checks to see if the inverse has already been cached.
+## If so, it returns the cached value. Otherwise it will calculate it.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {        
     inv <- x$getInv()
     if(!is.null(inv)){
       message ("getting cached data")
